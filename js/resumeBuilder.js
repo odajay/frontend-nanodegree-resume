15b89e3ceb7b20b25b7c8ff7d971ddfb8d3ee45d
Jonathan Gall <odajay@gmail.com>
2016-09-07 21:40:38 +0200

    Encapsulation and last changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,26 +4,26 @@ var work = {
         "title": "Product Designer",
         "location": "Kraków",
         "dates": "January 2014 - Now",
-        "description": "Prototyping, Visual Design, Interface Design, Interaction Design"
+        "description": "Prototyping of innovative financial application using Framer and Invision. Leading the Visual Design, Interface Design, Interaction Design."
     }, {
         "employer": "Brown Brothers Harriman",
         "title": "Reporting Specialist",
         "location": "Luxembourg, Kraków",
         "dates": "September 2010 - January 2014",
-        "description": "IBM Cognos 10, SAS, Efront, Business analysis"
+        "description": "Development of reports using IBM Cognos 10, SAS, Efront. Business analysis and redaction of specifications."
     }, {
         "employer": "Schroders Investment Management",
         "title": "Reporting Specialist",
         "location": "Luxembourg",
         "dates": "March 2007 - September 2010",
-        "description": "Business Objects, Business analysis"
+        "description": "Development of reports using Business Objects. Business analysis and redaction of specifications."
 
     }, {
         "employer": "Euriware, Amphaz",
         "title": "Reporting Specialist",
         "location": "Aix-en-Provence",
         "dates": "October 2005 - March 2007",
-        "description": "Business Objects, Business analysis"
+        "description": "Development of reports using Business Objects. Business analysis and redaction of specifications."
     }]
 };
 
@@ -36,25 +36,6 @@ var projects = {
         "images": ["images/screenshot1_tn.png", "images/screenshot2_tn.png"]
     }]
 };
-
-
-
-var bio = {
-    "name": "Jonathan Gall",
-    "role": "UX Designer and Front-End Developer",
-    "contacts": {
-        "mobile": "+48 535 203 086",
-        "email": "jonathanonline@gmail.com",
-        "github": "odajay",
-        "twitter": "@odajay",
-        "location": "Kielce"
-    },
-    "welcomeMessage": "I always loved to solve problems, so I found a way to make a living out of it. I am focusing in rapid prototyping, user experience research and design languages.",
-    "skills": ["Rapid Prototyping", "Information Architecture", "Data visualization", "Framer", "HTML/CSS/JS"],
-    "bioPic": "images/portrait.jpg"
-};
-
-
 
 var education = {
     "schools": [{
@@ -82,83 +63,160 @@ var education = {
     }]
 };
 
-var myName = HTMLheaderName.replace("%data%", bio.name);
-$("#header").append(myName);
-
-if (bio.skills.length > 0) {
-    $("#header").append(HTMLskillsStart);
-
-    var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-    $("#skills").append(formattedSkill);
-    var formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-    $("#skills").append(formattedSkill);
-    var formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-    $("#skills").append(formattedSkill);
-    var formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
-    $("#skills").append(formattedSkill);
+var bio = {
+    "name": "Jonathan Gall",
+    "role": "UX Designer and Front-End Developer",
+    "contacts": {
+        "mobile": "+48 535 203 086",
+        "email": "jonathanonline@gmail.com",
+        "github": "odajay",
+        "twitter": "@odajay",
+        "location": "Kielce"
+    },
+    "welcomeMessage": "I am focusing in rapid prototyping, user experience research and design languages.",
+    "skills": ["Rapid Prototyping", "Information Architecture", "Data visualization", "Framer", "HTML/CSS/JS/jQuery", "Automation with Grunt", "Team player"],
+    "biopic": "images/portrait.jpg"
 };
 
 
-function displayWork() {
-    for (job in work.jobs) {
-        $("#workExperience").append(HTMLworkStart);
+bio.display = function() {
 
-        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-        var formattedEmployerTitle = formattedEmployer + formattedTitle;
-        $(".work-entry:last").append(formattedEmployerTitle);
-        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-        $(".work-entry:last").append(formattedDates);
-        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-        $(".work-entry:last").append(formattedDescription);
+    var myRole = HTMLheaderRole.replace("%data%", bio.role);
+    $("#header").prepend(myRole);
+    var myName = HTMLheaderName.replace("%data%", bio.name);
+    $("#header").prepend(myName);
+
+    var contactInfo = [];
+
+    contactInfo.push(HTMLmobile.replace("%data%", bio.contacts.mobile));
+    contactInfo.push(HTMLemail.replace("%data%", bio.contacts.email));
+    contactInfo.push(HTMLgithub.replace("%data%", bio.contacts.github));
+    contactInfo.push(HTMLtwitter.replace("%data%", bio.contacts.twitter));
+    contactInfo.push(HTMLlocation.replace("%data%", bio.contacts.location));
+
+    for (i = 0; i < contactInfo.length; i++) {
+        $("#topContacts").append(contactInfo[i]);
+        $("#footerContacts").append(contactInfo[i]);
+    };
+
+    var mybioPic = HTMLbioPic.replace("%data%", bio.biopic);
+    $("#header").append(mybioPic);
+    var mywelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+    $("#header").append(mywelcomeMessage);
+
+
+
+
+    if (bio.skills.length > 0) {
+        $("#header").append(HTMLskillsStart);
+        bio.skills.forEach(function(val, i) {
+            var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+            $("#skills").append(formattedSkill);
+        });
     }
 };
 
+bio.display();
 
-displayWork();
+education.display = function() {
+    $("#education").append(HTMLschoolStart);
+
+    education.schools.forEach(function(val, i) {
+
+        var formattedschoolName = HTMLschoolName.replace("%data%", education.schools[i].name);
+        $(".education-entry:last").append(formattedschoolName);
+        var formattedschoolDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
+        $(".education-entry:last").append(formattedschoolDegree);
+        var formattedschoolDates = HTMLschoolDates.replace("%data%", education.schools[i].years);
+        $(".education-entry:last").append(formattedschoolDates);
+        var formattedschoolLocation = HTMLschoolLocation.replace("%data%", education.schools[i].city);
+        $(".education-entry:last").append(formattedschoolLocation);
+        var formattedschoolMajor = HTMLschoolMajor.replace("%data%", education.schools[i].majors);
+        $(".education-entry:last").append(formattedschoolMajor);
+    });
+
+    $(".education-entry:last").append(HTMLonlineClasses);
+    education.onlineCourses.forEach(function(val, i) {
+
+        var formattedonlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title);
+        $(".education-entry:last").append(formattedonlineTitle);
+
+        var formattedonlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school);
+        $(".education-entry:last").append(formattedonlineSchool);
+        var formattedsonlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[i].dates);
+        $(".education-entry:last").append(formattedsonlineDates);
+        var formattedonlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[i].url);
+        $(".education-entry:last").append(formattedonlineURL);
+    });
+};
+
+education.display();
+
+
+
+work.display = function() {
+    $("#workExperience").append(HTMLworkStart);
+    work.jobs.forEach(function(val, i) {
+
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[i].title);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+        $(".work-entry:last").append(formattedEmployerTitle);
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[i].dates);
+        $(".work-entry:last").append(formattedDates);
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[i].description);
+        $(".work-entry:last").append(formattedDescription);
+    });
+};
+work.display();
+
+
 
 projects.display = function() {
-    for (project in projects.projects) {
-        $("#projects").append(HTMLprojectStart);
+    $("#projects").append(HTMLprojectStart);
+    projects.projects.forEach(function(val, i) {
 
-        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+
+        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title);
         $(".project-entry:last").append(formattedTitle);
 
-        var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+        var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
         $(".project-entry:last").append(formattedDates);
-        var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+        var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
         $(".project-entry:last").append(formattedDescription);
 
-       if (projects.projects[project].images.length > 0) {
-            for (image in projects.projects[project].images) {
-                var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+        if (projects.projects[i].images.length > 0) {
+            projects.projects[i].images.forEach(function(val, y) {
+                var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[i].images[y]);
                 $(".project-entry:last").append(formattedImage);
 
-           }
+            });
         }
-    };
+    });
 };
-
 projects.display();
 
+// Event Exercise
+// $(document).click(function(loc) {
+//     // your code goes here
+//     var x = loc.pageX;
+//     var y = loc.pageY;
 
-$(document).click(function(loc) {
-    // your code goes here
-    var x = loc.pageX;
-    var y = loc.pageY;
+//     logClicks(x, y);
+// });
 
-    logClicks(x, y);
-});
 
-function inName(USname) {
-    var positionSpace = USname.indexOf(" ");
-    var lastName = USname.slice(positionSpace).toUpperCase()
-    var firstName = USname.slice(0, positionSpace)
-    return firstName + lastName
-};
+// Capitalization exercise
+// function inName(USname) {
+//     var positionSpace = USname.indexOf(" ");
+//     var lastName = USname.slice(positionSpace).toUpperCase()
+//     var firstName = USname.slice(0, positionSpace)
+//     return firstName + lastName
+// };
 
-var myInternationalization = internationalizeButton;
-$("#main:last").append(myInternationalization);
+// Internationalization exercise
+// var myInternationalization = internationalizeButton;
+// $("#main:last").append(myInternationalization);
 
 
 $("#mapDiv").append(googleMap);
